d5eed6bcd3fe121ca77396adb6e36641074e921f
mslmyrtd <mslmyrtd@gmail.com>
2024-11-27 23:49:56 +0300

    added gitignore file, first migrate and SqlServer connection vs

---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,12 @@
+namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }=String.Empty;
+        public string? Email { get; set; }=String.Empty;
+        public int Phone { get; set; }
+        public bool Subscribed { get; set; }
+
+    }
+}
